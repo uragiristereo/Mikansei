5ef41e6b1ea786aafd3e4d924d45aa6ec35e93a7
Agung Watanabe <raava@outlook.co.id>
2023-03-30 09:35:08 +0700

    Domain: Fix tags filtering on use cases

--- core/domain/src/main/java/com/uragiristereo/mikansei/core/domain/usecase/GetPostsUseCase.kt
+++ core/domain/src/main/java/com/uragiristereo/mikansei/core/domain/usecase/GetPostsUseCase.kt
@@ -1,19 +1,20 @@
 package com.uragiristereo.mikansei.core.domain.usecase
 
 import com.uragiristereo.mikansei.core.danbooru.repository.DanbooruRepository
+import com.uragiristereo.mikansei.core.database.dao.user.UserDao
+import com.uragiristereo.mikansei.core.database.dao.user.toUser
 import com.uragiristereo.mikansei.core.domain.entity.post.PostsResult
 import com.uragiristereo.mikansei.core.domain.entity.post.toPostList
 import com.uragiristereo.mikansei.core.model.Constants
 import com.uragiristereo.mikansei.core.model.danbooru.post.Post
 import com.uragiristereo.mikansei.core.model.result.Result
 import com.uragiristereo.mikansei.core.model.result.mapSuccess
-import com.uragiristereo.mikansei.core.preferences.PreferencesRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 
 class GetPostsUseCase(
     private val danbooruRepository: DanbooruRepository,
-    private val preferencesRepository: PreferencesRepository,
+    private val userDao: UserDao,
 ) {
     suspend operator fun invoke(
         tags: String,
@@ -28,17 +29,24 @@ class GetPostsUseCase(
                     else -> currentPosts
                 }
 
-                val preferences = preferencesRepository.flowData.first()
-                val filters = preferences.blacklistedTags
-                val ratingFilters = preferences.ratingFilters.getFilteredRatings()
+                val user = userDao.getActive()
+                    .first()
+                    .toUser()
+
+                val ratingFilters = user.postsRatingFilter.getFilteredRatings()
 
                 var posts = danbooruPosts.toPostList()
                 var canLoadMore = posts.size == Constants.POSTS_PER_PAGE
 
+                val showDeletedPosts = when {
+                    user.showDeletedPosts -> listOf(false, true)
+                    else -> listOf(false)
+                }
+
                 posts = posts.filter { post ->
-                    !filters.any { tag ->
+                    !user.blacklistedTags.any { tag ->
                         post.tags.contains(tag.lowercase())
-                    } && post.rating !in ratingFilters
+                    } && post.rating !in ratingFilters && !post.isBanned && post.isDeleted in showDeletedPosts
                 }
 
                 if (canLoadMore && posts.isEmpty()) {
--- core/domain/src/main/java/com/uragiristereo/mikansei/core/domain/usecase/GetTagsAutoCompleteUseCase.kt
+++ core/domain/src/main/java/com/uragiristereo/mikansei/core/domain/usecase/GetTagsAutoCompleteUseCase.kt
@@ -1,34 +1,30 @@
 package com.uragiristereo.mikansei.core.domain.usecase
 
 import com.uragiristereo.mikansei.core.danbooru.repository.DanbooruRepository
+import com.uragiristereo.mikansei.core.database.dao.user.UserDao
+import com.uragiristereo.mikansei.core.database.dao.user.toUser
 import com.uragiristereo.mikansei.core.domain.entity.tag.TagAutoComplete
 import com.uragiristereo.mikansei.core.domain.entity.tag.toTagAutoCompleteList
 import com.uragiristereo.mikansei.core.model.result.Result
 import com.uragiristereo.mikansei.core.model.result.mapSuccess
-import com.uragiristereo.mikansei.core.preferences.PreferencesRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 
 class GetTagsAutoCompleteUseCase(
     private val danbooruRepository: DanbooruRepository,
-    private val preferencesRepository: PreferencesRepository,
+    private val userDao: UserDao,
 ) {
     suspend operator fun invoke(query: String): Flow<Result<List<TagAutoComplete>>> {
         return danbooruRepository.getTagsAutoComplete(query)
             .mapSuccess { danbooruTags ->
-                val filters = preferencesRepository.flowData
+                val tags = danbooruTags.toTagAutoCompleteList()
+                val filters = userDao.getActive()
                     .first()
+                    .toUser()
                     .blacklistedTags
 
-                val tags = danbooruTags.toTagAutoCompleteList()
-
-                val filtered = when {
-                    filters.isNotEmpty() -> tags
-                        .filter { item ->
-                            item.value !in filters
-                        }
-
-                    else -> tags
+                val filtered = tags.filter { item ->
+                    item.value !in filters
                 }
 
                 filtered
