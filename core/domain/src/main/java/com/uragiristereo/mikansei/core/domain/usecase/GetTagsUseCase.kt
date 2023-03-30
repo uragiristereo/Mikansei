b6dc02dbe96e70e20869f063e638ad8520173d12
Agung Watanabe <raava@outlook.co.id>
2023-03-30 14:06:50 +0700

    Domain: Fix wrong filters for get tags use case

--- core/domain/src/main/java/com/uragiristereo/mikansei/core/domain/usecase/GetTagsUseCase.kt
+++ core/domain/src/main/java/com/uragiristereo/mikansei/core/domain/usecase/GetTagsUseCase.kt
@@ -1,22 +1,24 @@
 package com.uragiristereo.mikansei.core.domain.usecase
 
 import com.uragiristereo.mikansei.core.danbooru.repository.DanbooruRepository
+import com.uragiristereo.mikansei.core.database.dao.user.UserDao
+import com.uragiristereo.mikansei.core.database.dao.user.toUser
 import com.uragiristereo.mikansei.core.domain.entity.tag.Tag
 import com.uragiristereo.mikansei.core.domain.entity.tag.TagCategory
 import com.uragiristereo.mikansei.core.domain.entity.tag.toTag
 import com.uragiristereo.mikansei.core.model.result.Result
 import com.uragiristereo.mikansei.core.model.result.mapSuccess
-import com.uragiristereo.mikansei.core.preferences.PreferencesRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 
 class GetTagsUseCase(
     private val danbooruRepository: DanbooruRepository,
-    private val preferencesRepository: PreferencesRepository,
+    private val userDao: UserDao,
 ) {
     suspend operator fun invoke(tags: List<String>): Flow<Result<List<Tag>>> {
-        val filters = preferencesRepository.flowData
+        val filters = userDao.getActive()
             .first()
+            .toUser()
             .blacklistedTags
 
         return danbooruRepository.getTags(tags)
