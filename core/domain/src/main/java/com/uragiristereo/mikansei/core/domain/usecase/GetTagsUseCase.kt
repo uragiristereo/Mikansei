35806982811c42f87d13e65613de57c6ba676a00
Agung Watanabe <raava@outlook.co.id>
2023-03-30 09:30:45 +0700

    Domain: Sort tags by category

--- core/domain/src/main/java/com/uragiristereo/mikansei/core/domain/usecase/GetTagsUseCase.kt
+++ core/domain/src/main/java/com/uragiristereo/mikansei/core/domain/usecase/GetTagsUseCase.kt
@@ -2,6 +2,7 @@ package com.uragiristereo.mikansei.core.domain.usecase
 
 import com.uragiristereo.mikansei.core.danbooru.repository.DanbooruRepository
 import com.uragiristereo.mikansei.core.domain.entity.tag.Tag
+import com.uragiristereo.mikansei.core.domain.entity.tag.TagCategory
 import com.uragiristereo.mikansei.core.domain.entity.tag.toTag
 import com.uragiristereo.mikansei.core.model.result.Result
 import com.uragiristereo.mikansei.core.model.result.mapSuccess
@@ -20,11 +21,31 @@ class GetTagsUseCase(
 
         return danbooruRepository.getTags(tags)
             .mapSuccess { danbooruTags ->
-                danbooruTags.map { danbooruTag ->
+                val sortedTags = danbooruTags.map { danbooruTag ->
                     val tag = danbooruTag.toTag()
 
                     tag.copy(isBlacklisted = tag.name in filters)
                 }.sortedBy { it.name }
+
+                val artistTags = mutableListOf<Tag>()
+                val copyrightTags = mutableListOf<Tag>()
+                val characterTags = mutableListOf<Tag>()
+                val metaTags = mutableListOf<Tag>()
+                val generalTags = mutableListOf<Tag>()
+
+                sortedTags.forEach { tag ->
+                    when (tag.category) {
+                        TagCategory.ARTIST -> artistTags.add(tag)
+                        TagCategory.COPYRIGHT -> copyrightTags.add(tag)
+                        TagCategory.CHARACTER -> characterTags.add(tag)
+                        TagCategory.META -> metaTags.add(tag)
+                        else -> generalTags.add(tag)
+                    }
+                }
+
+                val arrangedTags = artistTags + copyrightTags + characterTags + metaTags + generalTags
+
+                arrangedTags
             }
     }
 }
