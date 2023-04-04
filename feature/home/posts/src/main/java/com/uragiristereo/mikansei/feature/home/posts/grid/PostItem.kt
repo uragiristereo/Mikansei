f2a073b97c955eb9d53d5096ec24f3627064a6a7
Agung Watanabe <raava@outlook.co.id>
2023-04-04 10:08:26 +0700

    Posts: Remove unused loaded state

--- feature/home/posts/src/main/java/com/uragiristereo/mikansei/feature/home/posts/grid/PostItem.kt
+++ feature/home/posts/src/main/java/com/uragiristereo/mikansei/feature/home/posts/grid/PostItem.kt
@@ -7,11 +7,7 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -42,8 +38,6 @@ internal fun PostItem(
                 onLongClick = onLongPress,
             ),
     ) {
-        var loaded by rememberSaveable { mutableStateOf(false) }
-
         PostPlaceholder()
 
         AsyncImage(
@@ -59,7 +53,6 @@ internal fun PostItem(
             },
             contentDescription = null,
             contentScale = ContentScale.Crop,
-            onSuccess = { loaded = true },
             modifier = Modifier.fillMaxSize(),
         )
 
