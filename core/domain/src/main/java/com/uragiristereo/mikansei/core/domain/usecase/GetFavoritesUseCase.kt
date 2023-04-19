15b3220292cc7b5146154384cf92a693d15e21f6
Agung Watanabe <raava@outlook.co.id>
2023-04-19 16:30:59 +0700

    Favorites: Use get posts use case instead of repository call

--- core/domain/src/main/java/com/uragiristereo/mikansei/core/domain/usecase/GetFavoritesUseCase.kt
+++ core/domain/src/main/java/com/uragiristereo/mikansei/core/domain/usecase/GetFavoritesUseCase.kt
@@ -1,39 +1,40 @@
 package com.uragiristereo.mikansei.core.domain.usecase
 
-import com.uragiristereo.mikansei.core.danbooru.repository.DanbooruRepository
 import com.uragiristereo.mikansei.core.database.dao.user.UserDao
 import com.uragiristereo.mikansei.core.database.dao.user.toUser
 import com.uragiristereo.mikansei.core.domain.entity.favorite.Favorite
-import com.uragiristereo.mikansei.core.domain.entity.post.toPost
 import com.uragiristereo.mikansei.core.model.result.Result
 import com.uragiristereo.mikansei.core.model.result.mapSuccess
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 
 class GetFavoritesUseCase(
-    private val danbooruRepository: DanbooruRepository,
     private val userDao: UserDao,
+    private val getPostsUseCase: GetPostsUseCase,
 ) {
     suspend operator fun invoke(): Flow<Result<Favorite>> {
         val activeUser = userDao.getActive().first().toUser()
 
-        return danbooruRepository.getPosts(tags = "ordfav:${activeUser.name}", page = 1)
-            .mapSuccess { danbooruPosts ->
-                val thumbnailUrl = when {
-                    danbooruPosts.isNotEmpty() -> {
-                        val firstPost = danbooruPosts.first().toPost()
+        return getPostsUseCase(
+            tags = "ordfav:${activeUser.name}",
+            page = 1,
+            currentPosts = listOf(),
+        ).mapSuccess { postsResult ->
+            val thumbnailUrl = when {
+                postsResult.posts.isNotEmpty() -> {
+                    val firstPost = postsResult.posts.first()
 
-                        firstPost.previewImage.url
-                    }
-
-                    else -> null
+                    firstPost.previewImage.url
                 }
 
-                Favorite(
-                    id = 0,
-                    name = "My favorites",
-                    thumbnailUrl = thumbnailUrl,
-                )
+                else -> null
             }
+
+            Favorite(
+                id = 0,
+                name = "My favorites",
+                thumbnailUrl = thumbnailUrl,
+            )
+        }
     }
 }
