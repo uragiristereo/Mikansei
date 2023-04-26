4b4c68b4ec3a3e2d07f017c81ca415a12ebeff48
Agung Watanabe <raava@outlook.co.id>
2023-04-26 14:29:43 +0700

    UI: Fix hold animation

--- core/ui/src/main/java/com/uragiristereo/mikansei/core/ui/animation/Hold.kt
+++ core/ui/src/main/java/com/uragiristereo/mikansei/core/ui/animation/Hold.kt
@@ -6,6 +6,14 @@ import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 
-fun holdIn(durationMillis: Int = 300): EnterTransition = fadeIn(animationSpec = tween(durationMillis))
+fun holdIn(durationMillis: Int = 300): EnterTransition = fadeIn(
+    animationSpec = tween(durationMillis),
+    initialAlpha = 1f,
+)
 
-fun holdOut(durationMillis: Int = 300): ExitTransition = fadeOut(animationSpec = tween(durationMillis))
+fun holdOut(durationMillis: Int = 300): ExitTransition = fadeOut(
+    animationSpec = tween(durationMillis),
+    // Refer: https://issuetracker.google.com/issues/192993290
+    // targetAlpha = 1f,
+    targetAlpha = 0.999f,
+)
