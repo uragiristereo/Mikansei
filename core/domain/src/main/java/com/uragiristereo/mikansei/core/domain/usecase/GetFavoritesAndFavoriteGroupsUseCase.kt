5ee9c6d8b2d5223e26beffc3c8fc7323be792ef6
Agung Watanabe <raava@outlook.co.id>
2023-04-27 12:05:15 +0700

    Model: Implement combine extension function for ResultFlow

--- core/domain/src/main/java/com/uragiristereo/mikansei/core/domain/usecase/GetFavoritesAndFavoriteGroupsUseCase.kt
+++ core/domain/src/main/java/com/uragiristereo/mikansei/core/domain/usecase/GetFavoritesAndFavoriteGroupsUseCase.kt
@@ -2,8 +2,8 @@ package com.uragiristereo.mikansei.core.domain.usecase
 
 import com.uragiristereo.mikansei.core.domain.entity.favorite.Favorite
 import com.uragiristereo.mikansei.core.model.result.Result
+import com.uragiristereo.mikansei.core.model.result.combineSuccess
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.combine
 
 class GetFavoritesAndFavoriteGroupsUseCase(
     private val getFavoritesUseCase: GetFavoritesUseCase,
@@ -11,26 +11,14 @@ class GetFavoritesAndFavoriteGroupsUseCase(
 ) {
     suspend operator fun invoke(): Flow<Result<List<Favorite>>> {
         return getFavoritesUseCase()
-            .combine(
-                getFavoriteGroupsUseCase(
+            .combineSuccess(
+                resultFlow = getFavoriteGroupsUseCase(
                     forceCache = false,
                     forceRefresh = true,
-                )
-            ) { result1, result2 ->
-                when {
-                    result1 is Result.Success && result2 is Result.Success -> {
-                        Result.Success(
-                            data = listOf(result1.data) + result2.data
-                        )
-                    }
-
-                    result1 is Result.Failed -> Result.Failed(result1.message)
-                    result2 is Result.Failed -> Result.Failed(result2.message)
-                    result1 is Result.Error -> Result.Error(result1.t)
-                result2 is Result.Error -> Result.Error(result2.t)
-
-                else -> Result.Error(IllegalStateException("Unexpected result types"))
-            }
-        }
+                ),
+                transform = { result1, result2 ->
+                    listOf(result1) + result2
+                },
+            )
     }
 }
