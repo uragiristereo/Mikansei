4bfbe4ac8f355efdb05b4e70d13d807e353a2bd6
Agung Watanabe <raava@outlook.co.id>
2023-09-21 10:59:05 +0700

    Product: Use reserved colors

--- core/product/src/main/java/com/uragiristereo/mikansei/core/product/theme/Color.kt
+++ core/product/src/main/java/com/uragiristereo/mikansei/core/product/theme/Color.kt
@@ -6,10 +6,10 @@ import androidx.compose.ui.graphics.Color
 val ProductPrimaryLight = Color(color = 0xFF4359AC)
 val ProductSecondaryLight = Color(color = 0xFFEBB9D1)
 val TextLight = Color(color = 0xFF1F1F1F)
-val BackgroundLight = Color(color = 0xFFFFFFFF)
+val BackgroundLight = Color.White
 val ProductPrimaryDark = Color(color = 0xFFB6C4FF)
 val ProductSecondaryDark = Color(color = 0xFFB1849A)
-val TextDark = Color(color = 0xFFFFFFFF)
-val BackgroundDark = Color(color = 0xFF212121)
-val BackgroundBlack = Color(color = 0xFF000000)
-val ScrimColor = Color(color = 0xFF121212).copy(alpha = DrawerDefaults.ScrimOpacity)
+val TextDark = Color.White
+val BackgroundDark = Color(color = 0xFF121212)
+val BackgroundBlack = Color.Black
+val ScrimColor = BackgroundDark.copy(alpha = DrawerDefaults.ScrimOpacity)
