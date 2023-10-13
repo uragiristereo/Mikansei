5fbb54403b3dbf8ab6c7c7be7f381c000072de65
Agung Watanabe <raava@outlook.co.id>
2023-10-13 18:35:03 +0700

    UI: Copy Accompanist Navigation Material With ModalBottomSheetLayout2 implementation

---
+++ core/ui/src/main/java/com/uragiristereo/mikansei/core/ui/modalbottomsheet/navigation/ExperimentalMaterialNavigationApi.kt
@@ -0,0 +1,5 @@
+package com.uragiristereo.mikansei.core.ui.modalbottomsheet.navigation
+
+@RequiresOptIn(message = "This APIs are experimental and may change in the future.")
+@Retention(AnnotationRetention.BINARY)
+annotation class ExperimentalMaterialNavigationApi
