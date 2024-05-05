41c09f498fc198cef9dcd61e70f49c0cc06f21ff
Agung Watanabe <52477630+uragiristereo@users.noreply.github.com>
2024-05-05 23:18:00 +0700

    Image: Copy tag to clipboard when long pressed

--- feature/image/src/main/java/com/uragiristereo/mikansei/feature/image/more/tags/MoreTagItem.kt
+++ feature/image/src/main/java/com/uragiristereo/mikansei/feature/image/more/tags/MoreTagItem.kt
@@ -1,7 +1,10 @@
 package com.uragiristereo.mikansei.feature.image.more.tags
 
+import android.os.Build
+import android.widget.Toast
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -15,6 +18,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.uragiristereo.mikansei.core.domain.module.danbooru.entity.Tag
@@ -37,6 +45,7 @@ internal fun MoreTagItem(
     )
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 internal fun MoreTagItem(
     tag: Tag,
@@ -44,6 +53,10 @@ internal fun MoreTagItem(
     onSelectedChange: (Boolean) -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val clipboardManager = LocalClipboardManager.current
+    val hapticFeedback = LocalHapticFeedback.current
+    val context = LocalContext.current
+
     val isLight = MaterialTheme.colors.isLight
 
     val tagColor = remember(tag, isLight) {
@@ -65,10 +78,20 @@ internal fun MoreTagItem(
             .width(IntrinsicSize.Max)
             .clip(RoundedCornerShape(size = 8.dp))
             .background(backgroundColor)
-            .clickable(
+            .combinedClickable(
                 onClick = {
                     onSelectedChange(!selected)
                 },
+                onLongClick = {
+                    clipboardManager.setText(AnnotatedString(tag.name))
+                    hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
+
+                    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.TIRAMISU) {
+                        Toast
+                            .makeText(context, "Tag copied to the clipboard!", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
             )
             .padding(
                 vertical = 6.dp,
