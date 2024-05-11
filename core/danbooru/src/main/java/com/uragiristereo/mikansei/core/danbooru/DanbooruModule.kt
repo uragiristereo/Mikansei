fa8754ad8a4914ae8ae38b7e3f486314dedfa97a
Agung Watanabe <52477630+uragiristereo@users.noreply.github.com>
2024-05-11 11:15:22 +0700

    Danbooru: Add DanbooruModule [1/2]

---
+++ core/danbooru/src/main/java/com/uragiristereo/mikansei/core/danbooru/DanbooruModule.kt
@@ -0,0 +1,14 @@
+package com.uragiristereo.mikansei.core.danbooru
+
+import com.uragiristereo.mikansei.core.danbooru.interceptor.DanbooruAuthInterceptor
+import com.uragiristereo.mikansei.core.danbooru.interceptor.DanbooruHostInterceptor
+import com.uragiristereo.mikansei.core.domain.module.danbooru.DanbooruRepository
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+val danbooruModule = module {
+    singleOf(::DanbooruRepositoryImpl) bind DanbooruRepository::class
+    singleOf(::DanbooruAuthInterceptor)
+    singleOf(::DanbooruHostInterceptor)
+}
