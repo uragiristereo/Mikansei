f3dc7b419f191895dfbd80e621f01e5bb761718e
Agung Watanabe <52477630+uragiristereo@users.noreply.github.com>
2024-06-08 11:05:04 +0700

    UI: Add version extension to Context

--- core/ui/src/main/java/com/uragiristereo/mikansei/core/ui/extension/Context.kt
+++ core/ui/src/main/java/com/uragiristereo/mikansei/core/ui/extension/Context.kt
@@ -1,9 +1,25 @@
 package com.uragiristereo.mikansei.core.ui.extension
 
 import android.content.Context
+import android.os.Build
 
-@Suppress("DEPRECATION")
 val Context.versionName: String
     get() = packageManager
-        .getPackageInfo(this.packageName, 0)
+        .getPackageInfo(packageName, 0)
         .versionName
+
+@Suppress("DEPRECATION")
+val Context.versionCode: Long
+    get() = when {
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.P -> {
+            packageManager
+                .getPackageInfo(packageName, 0)
+                .longVersionCode
+        }
+
+        else -> {
+            packageManager
+                .getPackageInfo(packageName, 0)
+                .versionCode.toLong()
+        }
+    }
