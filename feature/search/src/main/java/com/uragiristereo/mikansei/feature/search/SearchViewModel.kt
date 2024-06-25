32f56a19a681ed83d3224d8f2689f8c4bc4ba8ce
Agung Watanabe <52477630+uragiristereo@users.noreply.github.com>
2024-06-25 13:27:06 +0700

    Search: Implement BrowseChips into existing search feature

--- feature/search/src/main/java/com/uragiristereo/mikansei/feature/search/SearchViewModel.kt
+++ feature/search/src/main/java/com/uragiristereo/mikansei/feature/search/SearchViewModel.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.lifecycle.SavedStateHandle
@@ -13,20 +14,37 @@ import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.SavedStateHandleSaveableApi
 import androidx.lifecycle.viewmodel.compose.saveable
 import com.uragiristereo.mikansei.core.domain.module.danbooru.entity.Tag
+import com.uragiristereo.mikansei.core.domain.module.database.TagCategoryRepository
+import com.uragiristereo.mikansei.core.domain.module.search.BrowseChipType
+import com.uragiristereo.mikansei.core.domain.usecase.GenerateChipsFromTagsUseCase
 import com.uragiristereo.mikansei.core.domain.usecase.GetTagsAutoCompleteUseCase
+import com.uragiristereo.mikansei.core.domain.usecase.GetTagsUseCase
 import com.uragiristereo.mikansei.core.model.result.Result
 import com.uragiristereo.mikansei.core.ui.navigation.MainRoute
 import com.uragiristereo.mikansei.feature.search.state.SearchWordIndex
 import com.uragiristereo.serializednavigationextension.runtime.navArgsOf
 import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
-@OptIn(SavedStateHandleSaveableApi::class)
+@OptIn(SavedStateHandleSaveableApi::class, FlowPreview::class, ExperimentalCoroutinesApi::class)
 class SearchViewModel(
     savedStateHandle: SavedStateHandle,
+    private val tagCategoryRepository: TagCategoryRepository,
     private val getTagsAutoCompleteUseCase: GetTagsAutoCompleteUseCase,
+    private val generateChipsFromTagsUseCase: GenerateChipsFromTagsUseCase,
+    private val getTagsUseCase: GetTagsUseCase,
 ) : ViewModel() {
     private val tags = savedStateHandle.navArgsOf(MainRoute.Search()).tags
 
@@ -80,6 +98,52 @@ class SearchViewModel(
             }
     }
 
+    private var tagCategoriesJob: Job? = null
+
+    val browseChips = snapshotFlow { parsedQuery }
+        .distinctUntilChanged()
+        .flatMapLatest {
+            val tags = generateChipsFromTagsUseCase(it, emptyMap())
+            val flattenedTags = flattenTags(tags)
+
+            tagCategoryRepository.getCategories(flattenedTags).map { tagsMap ->
+                tags.map { tag ->
+                    when (tag) {
+                        is BrowseChipType.Single -> {
+                            when (tag) {
+                                is BrowseChipType.Single.Regular -> {
+                                    tag.copy(category = tagsMap[tag.tag])
+                                }
+
+                                is BrowseChipType.Single.Qualifier -> tag
+                            }
+                        }
+
+                        is BrowseChipType.Or -> {
+                            tag.copy(
+                                tags = tag.tags.map { orTag ->
+                                    if (orTag is BrowseChipType.Single.Regular) {
+                                        orTag.copy(category = tagsMap[orTag.tag])
+                                    } else {
+                                        orTag
+                                    }
+                                }
+                            )
+                        }
+                    }
+                }
+            }
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.Eagerly,
+            initialValue = null,
+        )
+
+    init {
+        collectBrowseChips()
+    }
+
     private fun getTags(term: String) {
         job?.cancel()
 
@@ -92,6 +156,7 @@ class SearchViewModel(
                         clear()
                         addAll(result.data)
                     }
+                    cacheTags(result.data)
 
                     errorMessage = null
                 }
@@ -168,7 +233,10 @@ class SearchViewModel(
 
         delimiters.forEachIndexed { index, delimiter ->
             nextIndexes[index] = when {
-                text.indexOf(char = delimiter, startIndex = position) != -1 -> text.indexOf(delimiter, position)
+                text.indexOf(char = delimiter, startIndex = position) != -1 -> {
+                    text.indexOf(delimiter, position)
+                }
+
                 else -> text.length
             }
         }
@@ -198,5 +266,54 @@ class SearchViewModel(
         val word = text.substring(startIndex = startIndex, endIndex = endIndex)
 
         return SearchWordIndex(word, startIndex, endIndex)
+    }
+
+    private fun collectBrowseChips() {
+        viewModelScope.launch {
+            browseChips
+                .filterNotNull()
+                .filter { it.isNotEmpty() }
+                .debounce(timeoutMillis = 500L)
+                .collect(::getTagsFromBrowseChips)
+        }
+    }
+
+    private fun getTagsFromBrowseChips(tags: List<BrowseChipType>) {
+        tagCategoriesJob?.cancel()
+
+        tagCategoriesJob = viewModelScope.launch {
+            val flattenedTags = flattenTags(tags)
+            val result = getTagsUseCase(flattenedTags)
+
+            if (result is Result.Success) {
+                cacheTags(result.data)
+            }
+        }
+    }
+
+    private suspend fun cacheTags(tags: List<Tag>) {
+        val tagsMap = tags.associate { it.name to it.category }
+        tagCategoryRepository.update(tagsMap)
+    }
+
+    private fun flattenTags(tags: List<BrowseChipType>): List<String> {
+        return tags
+            .filter { it !is BrowseChipType.Single.Qualifier }
+            .flatMap { browseChipType ->
+                when (browseChipType) {
+                    is BrowseChipType.Single -> {
+                        when (browseChipType) {
+                            is BrowseChipType.Single.Regular -> listOf(browseChipType.tag)
+                            is BrowseChipType.Single.Qualifier -> emptyList()
+                        }
+                    }
+
+                    is BrowseChipType.Or -> {
+                        browseChipType.tags
+                            .filterIsInstance<BrowseChipType.Single.Regular>()
+                            .map { it.tag }
+                    }
+                }
+            }
     }
 }
