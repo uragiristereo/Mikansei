7ee893fe61e560a16e8e82a5bc79f4d9a3f0aa4c
Agung Watanabe <52477630+uragiristereo@users.noreply.github.com>
2024-09-15 10:53:46 +0700

    Product: Fix ProductModalBottomSheetLayout dismiss touch area

--- core/product/src/main/java/com/uragiristereo/mikansei/core/product/component/ProductModalBottomSheetLayout.kt
+++ core/product/src/main/java/com/uragiristereo/mikansei/core/product/component/ProductModalBottomSheetLayout.kt
@@ -1,11 +1,14 @@
 package com.uragiristereo.mikansei.core.product.component
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.statusBars
@@ -17,6 +20,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -82,6 +86,20 @@ private fun ProductModalBottomSheetContent(
         contentAlignment = Alignment.BottomCenter,
         modifier = modifier.windowInsetsPadding(WindowInsets.navigationBars.only(WindowInsetsSides.Horizontal)),
     ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null,
+                    onClick = {
+                        scope.launch {
+                            sheetState.hide()
+                        }
+                    },
+                )
+        )
+
         Surface(
             color = MaterialTheme.colors.background.backgroundElevation(),
             shape = RoundedCornerShape(
