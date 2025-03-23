9eb17d5797843ce75d961e820b6b66ee41e32927
Agung Watanabe <52477630+uragiristereo@users.noreply.github.com>
2025-03-23 13:09:05 +0700

    Model: Fix scroll position doesn't reset after refresh (1/2)

--- core/model/src/main/java/com/uragiristereo/mikansei/core/model/Constants.kt
+++ core/model/src/main/java/com/uragiristereo/mikansei/core/model/Constants.kt
@@ -4,5 +4,6 @@ object Constants {
     const val STATE_KEY_INITIALIZED = "main.initialized"
     const val STATE_KEY_POSTS_SESSION_ID = "posts.session.id"
     const val KEY_LOAD_MORE_PROGRESS = "posts.post.load.more"
+    const val KEY_TOP_SPACER = "top.spacer"
     const val POSTS_PER_PAGE = 100
 }
