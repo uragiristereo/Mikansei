a04427450aba499a16fdc8a634e78d8541a663f9
Agung Watanabe <52477630+uragiristereo@users.noreply.github.com>
2026-03-25 17:07:58 +0700

    Danbooru: Update Favorite mapper and repository for sealed class

--- core/danbooru/src/main/java/com/uragiristereo/mikansei/core/danbooru/model/favorite/FavoriteMapper.kt
+++ core/danbooru/src/main/java/com/uragiristereo/mikansei/core/danbooru/model/favorite/FavoriteMapper.kt
@@ -2,15 +2,14 @@ package com.uragiristereo.mikansei.core.danbooru.model.favorite
 
 import com.uragiristereo.mikansei.core.domain.module.danbooru.entity.Favorite
 
-fun DanbooruFavoriteGroup.toFavorite(): Favorite {
-    return Favorite(
+fun DanbooruFavoriteGroup.toFavoriteGroup(): Favorite.Group {
+    return Favorite.Group(
         id = id,
         name = name.replace(oldChar = '_', newChar = ' '),
-        thumbnailUrl = null,
         postIds = postIds,
     )
 }
 
-fun List<DanbooruFavoriteGroup>.toFavoriteList(): List<Favorite> {
-    return map { it.toFavorite() }
+fun List<DanbooruFavoriteGroup>.toFavoriteGroupList(): List<Favorite.Group> {
+    return map { it.toFavoriteGroup() }
 }
