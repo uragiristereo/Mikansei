7c606fd883d96b2fe8c5a30bfd079db051bcafde
Agung Watanabe <52477630+uragiristereo@users.noreply.github.com>
2026-03-25 17:07:50 +0700

    Domain: Refactor Favorite entity to sealed class with Regular and Group subtypes

--- core/domain/src/main/java/com/uragiristereo/mikansei/core/domain/module/danbooru/entity/FavoriteGroupResult.kt
+++ core/domain/src/main/java/com/uragiristereo/mikansei/core/domain/module/danbooru/entity/FavoriteGroupResult.kt
@@ -1,6 +1,6 @@
 package com.uragiristereo.mikansei.core.domain.module.danbooru.entity
 
-data class FavoriteResult(
+data class FavoriteGroupResult(
     val isFromCache: Boolean,
-    val items: List<Favorite>,
+    val items: List<Favorite.Group>,
 )
